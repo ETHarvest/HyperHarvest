85496fb962845ebbed6e7fb599cffabba9b7943d
0xChijioke <chijokeogbuanya@gmail.com>
2024-09-08 08:13:06 +0100

    update: harvest page

---
+++ packages/nextjs/app/harvest/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+
+import Harvest from "~~/components/hyperharvest/Harvest"
+
+
+const page = () => {
+  return (
+    <div className="w-full h-full">
+        <Harvest />
+    </div>
+  )
+}
+
+export default page
